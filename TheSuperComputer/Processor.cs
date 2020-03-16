241fb285bbdbaadc9342507103bad1348c178d3e
vicluthfi <muhammad.alghifari@students.amikom.ac.id>
2020-03-16 21:30:23 +0700

    The Super Computer

---
+++ TheSuperComputer/Processor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSuperComputer
+{
+	class Processor
+	{
+		private String series;
+		private int core;
+		private int cache;
+
+		public Processor()
+		{
+
+		}
+
+		public void setSeries(String series)
+		{
+			this.series = series;
+		}
+
+		public void setCore(int core)
+		{
+			this.core = core;
+		}
+
+		public int getCore()
+		{
+			return this.core;
+		}
+
+		public void setCache(int cache)
+		{
+			this.cache = cache;
+		}
+
+		public int getCache()
+		{
+			return this.cache;
+		}
+
+		public override string ToString()
+		{
+			return $"Procesor series : {this.series}; Processor core : {this.core} ; Processor cache : {this.cache}";
+		}
+	}
+}
